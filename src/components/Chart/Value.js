b3bf1190196a69e234103b9393d5447dc353292d
= <webdev.jameslemire@gmail.com>
2017-12-14 13:31:30 -0700

    JL master - Update data set.

--- src/components/Chart/Value.js
+++ src/components/Chart/Value.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({ value }) => {
   return (
     <section className="Value__parent">
-      <span>${ value }</span>
+      <h1>${ value }</h1>
     </section>
   )
 }
