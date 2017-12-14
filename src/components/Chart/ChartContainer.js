a093182e9ba0e40b2238b6be5a5342e4f1bf4066
= <webdev.jameslemire@gmail.com>
2017-12-14 14:30:58 -0700

    JL master - Update variable name.

--- src/components/Chart/ChartContainer.js
+++ src/components/Chart/ChartContainer.js
@@ -4,24 +4,24 @@ import data from '../../utils/chart-data.json';
 
 export default class ChartContainer extends Component {
   toggleSelected( levels, target_index ) {
-    let total_copy = this.state.value;
+    let value_copy = this.state.value;
 
     if ( levels[ target_index ].selected === true ) return;
 
     levels.forEach( ( level, level_index ) => {
       if ( level_index === target_index ) {
         level.selected = true;
-        total_copy += ( level_index + 1 ) * 50;
+        value_copy += ( level_index + 1 ) * 50;
       } else {
         if ( level.selected ) {
-          total_copy -= ( level_index + 1 ) * 50;
+          value_copy -= ( level_index + 1 ) * 50;
         }
 
         level.selected = false;
       }
     });
 
-    this.setState({ data, value: total_copy });
+    this.setState({ data, value: value_copy });
   }
 
   constructor() {
