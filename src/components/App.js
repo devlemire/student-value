4a0650f42b405314a4a84bbcc736fe523b92bebf
= <webdev.jameslemire@gmail.com>
2017-12-14 11:06:02 -0700

    JL master - Chart component rendering.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Chart from './Chart';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         App
+        <Chart />
       </div>
     );
   }
---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <section className="Chart__parent">
+      Chart
+    </section>
+  )
+}
